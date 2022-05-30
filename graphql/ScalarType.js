78bee0d155f8d65858b37f946729792345349375
Bill <bill.yen@lakehouse.digital>
2022-05-30 16:46:59 +0800

    remove comment

--- graphql/ScalarType.js
+++ graphql/ScalarType.js
@@ -1,13 +1,14 @@
 const { GraphqlProvider } = require("../graphql-provider");
 
+// see https://www.apollographql.com/docs/apollo-server/schema/custom-scalars/  for details
 GraphqlProvider.addScalarType({
   name: "Date",
   description: "Date custom scalar type",
   /**@param {Date} value */
   serialize(value) {
-    return value.toISOString(); // Convert outgoing Date to integer for JSON
+    return value.toISOString();
   },
   parseValue(value) {
-    return new Date(parseInt(value)); // Convert incoming integer to Date
+    return new Date(parseInt(value));
   },
 });
