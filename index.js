4e78edb2593db519e93f3ffacb8498ca09bdb463
Bill <bill.yen@lakehouse.digital>
2022-05-30 14:15:13 +0800

    clean up index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { ApolloServer } = require("apollo-server");
-const jsonwebtoken = require("jsonwebtoken");
 const { GraphqlProvider } = require("./graphql-provider"); // make sure this is where the lib exist
-const User = require("./model/User");
+
 GraphqlProvider.load(process.cwd() + "/graphql"); // make sure this is where all the grapql files exist
 
 const server = new ApolloServer({
