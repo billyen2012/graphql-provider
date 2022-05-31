af5ddfd2cae9254229c322454d2a202dcfe003e1
billyen2012 <billyen2012@gmail.com>
2022-05-31 17:53:40 +0800

    add _middleware function

--- graphql-provider.js
+++ graphql-provider.js
@@ -7,13 +7,41 @@ let _resolver = { Query: {}, Mutation: {} };
 let defaultError = null;
 let globalBeforeResolve = () => {};
 let globalAfterResolve = () => {};
+let _middleware_ = null;
+
+const fileLoader = (path) => {
+  const files = fs.readdirSync(path);
+  // if there is middleware add set it to var _middleware_
+  // , where it will be cache for each layer of files for the resolvers
+  const middlewareFileName = "_middleware.js";
+  if (files.includes(middlewareFileName)) {
+    const middlewareIndex = files.indexOf(middlewareFileName);
+    _middleware_ = require(`${path}/${files[middlewareIndex]}`);
+  } else {
+    _middleware_ = null;
+  }
+  const directories = [];
+  // start loading files
+  files.forEach((file) => {
+    const fileState = fs.statSync(`${path}/${file}`);
+    if (fileState.isFile()) return require(`${path}/${file}`);
+    if (fileState.isDirectory()) return directories.push(file);
+  });
+  // then load sub directories recursively
+  directories.forEach((directory) => {
+    fileLoader(`${path}/${directory}`);
+  });
+};
 
 const resolverFunction = ({
   resolver = () => {},
   beforeResolve = () => {},
   afterResolve = () => {},
+  _middleware = () => {},
   onError = null,
 }) => {
+  // cache middleware per layer of files loader
+  if (_middleware_) _middleware = _middleware_;
   return async (parent, args, context, info) => {
     try {
       // global before resolve
@@ -25,6 +53,11 @@ const resolverFunction = ({
       );
 
       if (globalBeforeResolveResponse) return globalBeforeResolveResponse;
+
+      // middleware before enter to local
+      const middlewareResponse = await _middleware(parent, args, context, info);
+
+      if (middlewareResponse) return middlewareResponse;
 
       // local before resolve
       const beforeResolveResponse = await beforeResolve(
@@ -208,10 +241,7 @@ const GraphqlProvider = {
   },
   load(path) {
     // load all the files under the path
-    const files = fs.readdirSync(path);
-    for (let file of files) {
-      require(`${path}/${file}`);
-    }
+    fileLoader(path);
     // push root type to the front
     const hasMutation = Object.keys(_resolver.Mutation).length > 0;
     const hasQuery = Object.keys(_resolver.Query).length > 0;
