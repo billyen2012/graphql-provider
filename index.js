c1200930899e7dc3a3377083bdee12ee52ac8289
Bill <bill.yen@lakehouse.digital>
2022-06-03 16:26:59 +0800

    update

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const { GraphqlProvider } = require("./graphql-provider"); // make sure this is where the lib exist
 const { errorHanlder } = require("./lib/error");
+const Article = require("./model/Article");
+const User = require("./model/User");
 
-GraphqlProvider.load(process.cwd() + "/graphql"); // make sure this is where all the grapql files exist
+const start = async () => {
+  // syn database
+  await User.sync();
+  await Article.sync();
+  // load graphql
+  GraphqlProvider.load(process.cwd() + "/graphql"); // make sure this is where all the grapql files exist
+  // server start
+  GraphqlProvider.start().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
+};
 
-// GraphqlProvider.beforeResolve(() => {
-//   console.log("global before resolved called");
-// });
+// global event hook
+GraphqlProvider.beforeResolve(() => {
+  console.log("global before resolved called");
+});
 
-// GraphqlProvider.afterResolve(() => {
-//   console.log("global after resolved called");
-// });
+GraphqlProvider.afterResolve(() => {
+  console.log("global after resolved called");
+});
 
 // if local onError is not defined, this will be called
 // make sure err is only the subclass of Error object
@@ -19,6 +32,4 @@ GraphqlProvider.onError((err) => {
   errorHanlder(err);
 });
 
-GraphqlProvider.start().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+start();
